64bf5531da00b047b00099abd6755b48d47cc4dd
Ashish Virmani <virmani.ashish@gmail.com>
2012-12-30 14:04:15 -0800

    Enabled rdio functionality to the thumbUp button on each song slide

--- content_script.js
+++ content_script.js
@@ -1,6 +1,28 @@
-$(".thumbUpButton").click(function() {
-	var title = $(".playerBarSong").text();
-	var artist = $(".playerBarArtist").text();
+var insertListener = function(event){
+  if (event.animationName == "nodeInserted") {
+    var slides = $(".slide");
+    if(slides.length >= 2) {
+        var slide = $(slides[1]);
+        var selector = slide.find(".thumbUp");
 
-	chrome.extension.sendMessage({"artist": artist, "title": title});
+        var $hiddenSongTitle = $('<input/>',{type:'hidden',class:"rdioSongTitle",value:$(".playerBarSong").text()});
+        var $hiddenArtist = $('<input/>',{type:'hidden',class:"rdioArtist",value:$(".playerBarArtist").text()});
+        selector.append($hiddenSongTitle);
+        selector.append($hiddenArtist);
+    }
+  }
+}
+
+document.addEventListener("webkitAnimationStart", insertListener, false); // Chrome + Safari
+
+$('.slides').on('click', '.thumbUp', function(event){
+    var title = $(event.target.parentNode).find(".rdioSongTitle").attr("value");
+    var artist = $(event.target.parentNode).find(".rdioArtist").attr("value");
+    chrome.extension.sendMessage({"artist": artist, "title": title});
+});
+
+$(".thumbUpButton").click(function() {
+    var title = $(".playerBarSong").text();
+    var artist = $(".playerBarArtist").text();
+    chrome.extension.sendMessage({"artist": artist, "title": title});
 });
